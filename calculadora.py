bfa42b7e3934b05059e8dfd5785bc3dd9bd313ef
japinha2008 <nicole.abdala@escola.pr.gov.br>
2026-04-17 09:19:40 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+# Declara as variáveis numéricas
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Configura a lógica das operações
+soma = num1 + num2
+subtracao = num1 - num2
+
+# Exibe os resultados
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
